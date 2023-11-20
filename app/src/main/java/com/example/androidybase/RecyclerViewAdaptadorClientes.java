fd96e62e5423ae1902a63532b785bf9753cb6af8
Deraybcbc <a22kevburcac@inspedralbes.cat>
2023-11-20 13:22:58 +0100

    Modificaciones

--- app/src/main/java/com/example/androidybase/RecyclerViewAdaptadorClientes.java
+++ app/src/main/java/com/example/androidybase/RecyclerViewAdaptadorClientes.java
@@ -1,31 +1,51 @@
 package com.example.androidybase;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdaptadorClientes extends RecyclerView.Adapter<RecyclerViewAdaptadorClientes.ViewHoldder> {
+
+public class RecyclerViewAdaptadorClientes extends RecyclerView.Adapter<RecyclerViewAdaptadorClientes.ViewHolder> {
 
     @Override
-    public RecyclerViewAdaptadorClientes.ViewHoldder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
-    }
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_clientes, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;    }
 
     @Override
-    public void onBindViewHolder(RecyclerViewAdaptadorClientes.ViewHoldder holder, int position) {
+    public void onBindViewHolder(RecyclerViewAdaptadorClientes.ViewHolder holder, int position) {
 
     }
+/*
+    public RecyclerViewAdaptadorClientes(List<> productos) {
+        //this.productos = productos;
 
+    }
+*/
     @Override
     public int getItemCount() {
         return 0;
     }
 
-    public class ViewHoldder extends RecyclerView.ViewHolder {
-        public ViewHoldder(@NonNull View itemView) {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView infoNom,infoApe,infoUser,infocontra,infoDire,infotel,infoEmail,infoFecha;
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            infoNom = (TextView) itemView.findViewById(R.id.InfoNom);
+            infoApe = (TextView) itemView.findViewById(R.id.infoApe);
+            infoUser = (TextView) itemView.findViewById(R.id.InfoUser);
+            infocontra = (TextView) itemView.findViewById(R.id.infoCon);
+            infoDire = (TextView) itemView.findViewById(R.id.infoDire);
+            infotel = (TextView) itemView.findViewById(R.id.infoTel);
+            infoEmail = (TextView) itemView.findViewById(R.id.infoEmail);
+            infoFecha = (TextView) itemView.findViewById(R.id.infoFecha);
+
         }
     }
 }
